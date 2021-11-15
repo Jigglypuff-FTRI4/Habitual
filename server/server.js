b7414030e16b477f4f0f010ac32b69bfe9cf4fad
annechanchan <annechan017@gmail.com>
2021-11-15 14:59:20 -0800

    setup webpack and package json

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.use('/dist', express.static(path.join(__dirname, '../dist')))
+
+app.get('/', (req, res) => {
+  return res.status(200).type('html').sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 400,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
+
+
+app.listen(PORT, () => {console.log(`Server listening on port ${PORT}`)});
