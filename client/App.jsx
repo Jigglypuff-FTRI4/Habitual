18acc7a55d6f1b8e05b6fe473abdf2ab37e3481b
jdavis <jacob.drew.davis@gmail.com>
2021-11-16 22:01:07 -0600

    added navbar to app

--- client/App.jsx
+++ client/App.jsx
@@ -11,12 +11,17 @@
 
 import React, { Component } from "react";
 import { render } from "react-dom";
+import NavBar from "./common/NavBar.jsx";
 import Home from "./containers/Home.jsx";
+import { useTheme } from '@mui/material/styles'
 
 export default function App() {
+  const theme = useTheme();
+  theme.palette.mode = 'dark';
   return (
-    <div>
-      <h1>this is the App component</h1>
+    <div id="app">
+      <NavBar />
+      {/* <h1>this is the App component</h1> */}
       <Home />
     </div>
   );
