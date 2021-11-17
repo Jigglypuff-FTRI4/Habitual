8c32d6ec21d72db1feb5541d3642c8968ec2fa3f
annechanchan <annechan017@gmail.com>
2021-11-16 23:28:33 -0800

    in the middle of creating calendar

--- client/containers/CalendarView.jsx
+++ client/containers/CalendarView.jsx
@@ -1,47 +1,84 @@
 import React, { useCallback, useEffect, useState } from "react";
 import Calendar from '../components/Calendar.jsx';
-
+import moment from 'moment'
 import '../styles/containers/CalendarView.scss'
 
-//Dummy Data//
-const calendarInfo = [
-  { mood: 1, comment: 'test'},
-  { mood: 2, comment: 'test'},
-  { mood: 3, comment: 'test'},
-  { mood: 4, comment: 'test'},
-  { mood: 5, comment: 'test'},
-  { mood: 6, comment: 'test'},
-  { mood: 7, comment: 'test'},
-  { mood: 8, comment: 'test'},
-  { mood: 9, comment: 'test'},
-  { mood: 10, comment: 'test'},
-  { mood: 1, comment: 'test'},
-  { mood: 2, comment: 'test'},
-  { mood: 3, comment: 'test'},
-  { mood: 4, comment: 'test'},
-  { mood: 5, comment: 'test'},
-  { mood: undefined, comment: 'test'},
-  { mood: 7, comment: 'test'},
-  { mood: 8, comment: 'test'},
-  { mood: 9, comment: 'test'},
-  { mood: undefined, comment: 'test'},
-  { mood: 10, comment: 'test'},
-];
+export default function Calender () {
+  const value = moment();
+  //first day of the calendar month
+  const startDay=value.clone().startOf('month').startOf('week');
+  //last calendar day of the calendar month
+  const endDay=value.clone().endOf('month').endOf('week');
+  //set iterator day 1 day before the calendar month
+  const day = startDay.clone().subtract(1,"day");
+  const calendarArray =[];
 
+  //isBefore is a method from moment  
+  while (day.isBefore(endDay, 'day')){
+    calendarArray.push(
+      //newArray with 7 values, map over the 7 zeros
+      Array(7).fill(0).map(() => day.add(1, 'day').clone())
+    )
+  }
 
-export default function CalendarView (props) {
-  //STATE//
-  const [calendarData, setCalendarData] = useState(calendarInfo)
-  console.log('calendarData:', calendarData);
-
-
-  return (
-      <div id='calendar-view'>
-      <Calendar
-        calendarData={calendarData}
-      />
-      
-      </div>
+  const calendar = calendarArray.map((week) => 
+    <div className="calendar">
+      {week.map((day) => 
+      <div className="day">
+        {day.format('D')}
+      </div>)}
+    </div>
   )
 
-};
+  return (
+    <div>
+      {calendar}
+    </div>
+  )
+}
+
+
+
+
+// //Dummy Data//
+// const calendarInfo = [
+//   { mood: 1, comment: 'test'},
+//   { mood: 2, comment: 'test'},
+//   { mood: 3, comment: 'test'},
+//   { mood: 4, comment: 'test'},
+//   { mood: 5, comment: 'test'},
+//   { mood: 6, comment: 'test'},
+//   { mood: 7, comment: 'test'},
+//   { mood: 8, comment: 'test'},
+//   { mood: 9, comment: 'test'},
+//   { mood: 10, comment: 'test'},
+//   { mood: 1, comment: 'test'},
+//   { mood: 2, comment: 'test'},
+//   { mood: 3, comment: 'test'},
+//   { mood: 4, comment: 'test'},
+//   { mood: 5, comment: 'test'},
+//   { mood: undefined, comment: 'test'},
+//   { mood: 7, comment: 'test'},
+//   { mood: 8, comment: 'test'},
+//   { mood: 9, comment: 'test'},
+//   { mood: undefined, comment: 'test'},
+//   { mood: 10, comment: 'test'},
+// ];
+
+
+// export default function CalendarView (props) {
+//   //STATE//
+//   const [calendarData, setCalendarData] = useState(calendarInfo)
+//   console.log('calendarData:', calendarData);
+
+
+//   return (
+//       <div id='calendar-view'>
+//       <Calendar
+//         calendarData={calendarData}
+//       />
+      
+//       </div>
+//   )
+
+// };
